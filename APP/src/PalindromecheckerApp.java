0dd03a5a404a58d233561b0487695352c691dbad
dfg <gfd.gmail.com>
2026-02-27 09:49:00 +0530

    chl bsdk

---
+++ APP/src/PalindromecheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromecheckerApp {
+    static void main() {
+        System.out.println("welcome");
+        System.out.println("version:1.0");
+        System.out.println("system initialise");
+    }
+}
